44278eb18a27bbebf22b4115c5c5df333641c2e9
suzhongde <2373544536@qq.com>
2023-06-05 10:55:47 +0800

    完成excel转json学习

--- encryptedCollection/JSONparsing/demo.js
+++ encryptedCollection/JSONparsing/demo.js
@@ -316,3 +316,36 @@
 //
 //     fs.writeFileSync('name.xlsx', xls, 'binary');
 // })
+
+
+// excel -> JSON 代码：
+// npm install node-xlsx
+// const xlsx = require("node-xlsx");
+// const fs = require("fs");
+// // 读取xlsx
+// const sheets = xlsx.parse("./name.xlsx");
+// // 获取xlsx第一个标签栏的数据
+// const sheetData = sheets[0].data;
+// // 定义数据列表
+// let testList = [];
+// // 循环拼装数据
+// sheetData.forEach((item, index) => {
+//     if (index == 0) {
+//         // 去除标题栏
+//         return;
+//     } else {
+//         testList.push({
+//             name: item[0],
+//             lng: item[1],
+//             lat: item[2],
+//             height: item[3],
+//         });
+//     }
+// });
+// const jsonObj = testList;
+// // 不压缩的情况
+// // fs.writeFileSync('test-shujuchouqu.json',JSON.stringify(jsonObj, null, "\t"));
+// // 压缩的情况
+// fs.writeFileSync("yuanyouguanxian.json", JSON.stringify(jsonObj));
+// console.log('文件转换完成')
+
